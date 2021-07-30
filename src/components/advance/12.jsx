75e854a2ae33d9d955a1a6ce37c317fcc761231b
Jayesh Garg <jayesh.garg@workhuman.com>
2021-07-30 19:28:06 +0100

    change listener, value grabing handle form submission

--- src/components/advance/12.jsx
+++ src/components/advance/12.jsx
@@ -2,16 +2,26 @@ import React, { useEffect, useState } from 'react'
 import 'regenerator-runtime'
 
 const FormBasicsExample = () => {
+    const [firstName, setFirstName] = useState('Enter your first name')
+    const [lastName, setLastName] = useState('Enter your last name')
+    const [email, setEmail] = useState('Enter your email address')
+
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(e)
+        const name = e.target.localName + ' ' + e.target.nodeName
+        console.log(name)
+        const elements = [...e.target] //extracting form elements out of target
+        elements.map((ele) => {
+            console.log(ele.id + ':' + ele.value)
+        })
     }
     return (
         <>
             <h2>Forms Example</h2>
             <article>
-                <form>
-                    <div className='mb-5'>
+                <form onSubmit={handleSubmit} className='row'>
+                    <div className='mb-5 col-md-4'>
                         <label htmlFor='firstName' className='form-label'>
                             First Name
                         </label>
@@ -20,9 +30,13 @@ const FormBasicsExample = () => {
                             type='text'
                             id='firstName'
                             name='firstName'
+                            placeholder={firstName}
+                            onChange={(e) => {
+                                setFirstName(e.target.value)
+                            }}
                         />
                     </div>
-                    <div className='mb-5'>
+                    <div className='mb-5 col-md-4'>
                         <label htmlFor='lastName' className='form-label'>
                             Last Name
                         </label>
@@ -31,9 +45,13 @@ const FormBasicsExample = () => {
                             type='text'
                             id='lastName'
                             name='lastName'
+                            placeholder={lastName}
+                            onChange={(e) => {
+                                setLastName(e.target.value)
+                            }}
                         />
                     </div>
-                    <div className='mb-5'>
+                    <div className='mb-5 col-md-4'>
                         <label htmlFor='email' className='form-label'>
                             E-Mail
                         </label>
@@ -42,13 +60,16 @@ const FormBasicsExample = () => {
                             type='text'
                             id='email'
                             name='email'
+                            placeholder={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}
                         />
                     </div>
                     <div className='mb-5'>
                         <button
                             className='btn btn-primary form-control'
                             type='submit'
-                            onClick={handleSubmit}
                         >
                             Submit
                         </button>
