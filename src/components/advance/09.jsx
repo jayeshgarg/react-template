624ac855696e114c166b38712ba67405c4e3d932
Jayesh Garg <jayesh.garg@workhuman.com>
2021-07-30 11:30:39 +0100

    Conditional rendering multiple return fetch data

--- src/components/advance/09.jsx
+++ src/components/advance/09.jsx
@@ -4,22 +4,48 @@ import 'regenerator-runtime'
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const ConditionalRenderingMultipleReturnsExample = () => {
-    const [loading, setLoading] = useState(true)
-
-    const loadUser = async () => {
-        const response = await fetch(url)
-        const user = await response.json()
-        console.log(user)
-        setLoading(false)
-    }
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+    const [user, setUser] = useState('Default User')
 
     useEffect(() => {
-        loadUser()
+        setIsLoading(true)
+        setIsError(false)
+        //loading the data in useEffect instead of writing a new function
+        fetch(url)
+            .then((resp) => {
+                console.log(resp.status)
+                if (resp.status == 200) {
+                    return resp.json()
+                } else {
+                    throw new Error('Error from server')
+                }
+            })
+            .then((user) => {
+                console.log(user)
+                setUser(user.login)
+                setIsError(false)
+                setIsLoading(false)
+                return user
+            })
+            .catch((err) => {
+                setIsError(true)
+                setIsLoading(false)
+                console.log(err)
+            })
     }, [])
-    if (loading) {
+    if (isLoading) {
+        console.log('loading...')
         return <h1>Loading</h1>
     } else {
-        return <h1>Loaded</h1>
+        if (isError) {
+            console.log('error came...')
+            return <h1>Error</h1>
+        } else {
+            console.log('data loaded...')
+            console.log(user)
+            return <h1>{user}</h1>
+        }
     }
 }
 
